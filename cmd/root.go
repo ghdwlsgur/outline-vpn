b62b952b76752ac92319cfa6c589ea16e8565940
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-08 16:17:26 +0900

    govpn root module

---
+++ cmd/root.go
@@ -0,0 +1,256 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"govpn/internal"
+	"os"
+	"sort"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	_defaultProfile = "default"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "govpn",
+		Short: `govpn is interactive CLI tool`,
+		Long:  `govpn is interactive CLI tool`,
+	}
+
+	_credential              *Credential
+	_credentialWithMFA       = fmt.Sprintf("%s_mfa", config.DefaultSharedConfigFilename())
+	_credentialWithTemporary = fmt.Sprintf("%s_temporary", config.DefaultSharedCredentialsFilename())
+
+	defaultAwsRegions = []string{
+		"af-south-1",
+		"ap-east-1", "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-south-1", "ap-southeast-2", "ap-southeast-3",
+		"ca-central-1",
+		"cn-north-1", "cn-northwest-1",
+		"eu-central-1", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3",
+		"me-south-1",
+		"sa-east-1",
+		"us-east-1", "us-east-2", "us-gov-east-1", "us-gov-west-2", "us-west-1", "us-west-2",
+	}
+)
+
+type (
+	Region struct {
+		Name string
+	}
+)
+
+type Credential struct {
+	awsProfile string
+	awsConfig  *aws.Config
+}
+
+func Execute(version string) {
+	rootCmd.Version = version
+	if err := rootCmd.Execute(); err != nil {
+		panicRed(err)
+	}
+}
+
+func panicRed(err error) {
+	fmt.Println(color.RedString("[err] %s", err.Error()))
+	os.Exit(1)
+}
+
+func AskRegion(ctx context.Context, cfg aws.Config) (*Region, error) {
+	var regions []string
+	client := ec2.NewFromConfig(cfg)
+
+	output, err := client.DescribeRegions(ctx, &ec2.DescribeRegionsInput{
+		AllRegions: aws.Bool(true),
+	})
+	if err != nil {
+		regions = make([]string, len(defaultAwsRegions))
+		copy(regions, defaultAwsRegions)
+	} else {
+		regions = make([]string, len(output.Regions))
+		for _, region := range output.Regions {
+			regions = append(regions, aws.ToString(region.RegionName))
+		}
+	}
+	sort.Strings(regions)
+
+	var region string
+	prompt := &survey.Select{
+		Message: "Choose a region in AWS:",
+		Options: regions,
+	}
+
+	if err := survey.AskOne(prompt, &region, survey.WithIcons(func(icons *survey.IconSet) {
+		icons.SelectFocus.Format = "green+hb"
+	}), survey.WithPageSize(20)); err != nil {
+		return nil, err
+	}
+
+	return &Region{Name: region}, nil
+}
+
+func initConfig() {
+	_credential = &Credential{}
+
+	awsProfile := viper.GetString("profile")
+	if awsProfile == "" {
+		if os.Getenv("AWS_PROFILE") != "" {
+			awsProfile = os.Getenv("AWS_PROFILE")
+		} else {
+			awsProfile = _defaultProfile
+		}
+	}
+	_credential.awsProfile = awsProfile
+
+	awsRegion := viper.GetString("region")
+
+	sharedCredFile := os.Getenv("AWS_SHARED_CREDENTIALS_FILE")
+	if sharedCredFile == "" {
+		if _, err := os.Stat(_credentialWithMFA); !os.IsNotExist(err) {
+			color.Yellow("[Use] gossm default mfa credential file %s", _credentialWithMFA)
+			os.Setenv("AWS_SHARED_CREDENTIALS_FILE", _credentialWithMFA)
+			sharedCredFile = _credentialWithMFA
+		}
+	}
+
+	if sharedCredFile != "" {
+		awsConfig, err := internal.NewSharedConfig(context.Background(),
+			_credential.awsProfile,
+			[]string{config.DefaultSharedConfigFilename()},
+			[]string{sharedCredFile},
+		)
+		if err != nil {
+			panicRed(internal.WrapError(err))
+		}
+
+		cred, err := awsConfig.Credentials.Retrieve(context.Background())
+
+		if err != nil || cred.Expired() || cred.AccessKeyID == "" || cred.SecretAccessKey == "" {
+			color.Yellow("[Expire] govpn default mfa credential file %s", sharedCredFile)
+			os.Unsetenv("AWS_SHARED_CREDENTIALS_FILE")
+		} else {
+			_credential.awsConfig = &awsConfig
+		}
+	}
+
+	args := os.Args[1:]
+	subcmd, _, err := rootCmd.Find(args)
+	if err != nil {
+		panicRed(internal.WrapError(err))
+	}
+
+	switch subcmd.Use {
+	case "mfa":
+		if _credential.awsConfig != nil {
+			cred, err := _credential.awsConfig.Credentials.Retrieve(context.Background())
+			if err != nil {
+				panicRed(internal.WrapError(err))
+			}
+
+			if cred.SessionToken != "" {
+				os.Unsetenv("AWS_SHARED_CREDENTIALS_FILE")
+				_credential.awsConfig = nil
+			}
+		}
+	}
+
+	if _credential.awsConfig == nil {
+		var temporaryCredentials aws.Credentials
+		var temporaryConfig aws.Config
+
+		if os.Getenv("AWS_ACCESS_KEY_ID") != "" && os.Getenv("AWS_SECRET_ACCESS_KEY") != "" {
+			temporaryConfig, err = internal.NewConfig(context.Background(),
+				os.Getenv("AWS_ACCESS_KEY_ID"), os.Getenv("AWS_SECRET_ACCESS_KEY"),
+				os.Getenv("AWS_SESSION_TOKEN"), awsRegion, os.Getenv("AWS_ROLE_ARN"))
+			if err != nil {
+				panicRed(internal.WrapError(err))
+			}
+
+			temporaryCredentials, err = temporaryConfig.Credentials.Retrieve(context.Background())
+			if err != nil || temporaryCredentials.Expired() ||
+				temporaryCredentials.AccessKeyID == "" || temporaryCredentials.SecretAccessKey == "" ||
+				(subcmd.Use == "mfa" && temporaryCredentials.SessionToken != "") {
+				panicRed(internal.WrapError(fmt.Errorf("[err] invalid global environments %s", err.Error())))
+			}
+		} else {
+			temporaryConfig, err = internal.NewSharedConfig(context.Background(), _credential.awsProfile,
+				[]string{config.DefaultSharedConfigFilename()}, []string{})
+			if err == nil {
+				temporaryCredentials, err = temporaryConfig.Credentials.Retrieve(context.Background())
+			}
+
+			if err != nil || temporaryCredentials.Expired() ||
+				temporaryCredentials.AccessKeyID == "" || temporaryCredentials.SecretAccessKey == "" ||
+				(subcmd.Use == "mfa" && temporaryCredentials.SessionToken != "") {
+
+				temporaryConfig, err = internal.NewSharedConfig(context.Background(), _credential.awsProfile,
+					[]string{config.DefaultSharedConfigFilename()}, []string{config.DefaultSharedCredentialsFilename()})
+
+				if err != nil {
+					panicRed(internal.WrapError(err))
+				}
+
+				temporaryCredentials, err = temporaryConfig.Credentials.Retrieve(context.Background())
+				if err != nil {
+					panicRed(internal.WrapError(err))
+				}
+				if temporaryCredentials.Expired() || temporaryCredentials.AccessKeyID == "" || temporaryCredentials.SecretAccessKey == "" {
+					panicRed(internal.WrapError(fmt.Errorf("[err] not found credentials")))
+				}
+
+				if awsRegion == "" {
+					awsRegion = temporaryConfig.Region
+				}
+			}
+		}
+
+		temporaryCredentialsString := fmt.Sprintf(mfaCredentialFormat, _credential.awsProfile, temporaryCredentials.AccessKeyID,
+			temporaryCredentials.SecretAccessKey, temporaryCredentials.SessionToken)
+		if err := os.WriteFile(_credentialWithTemporary, []byte(temporaryCredentialsString), 0600); err != nil {
+			panicRed(internal.WrapError(err))
+		}
+
+		os.Setenv("AWS_SHARED_CREDENTIALS_FILE", _credentialWithTemporary)
+		awsConfig, err := internal.NewSharedConfig(context.Background(),
+			_credential.awsProfile, []string{}, []string{_credentialWithTemporary},
+		)
+		if err != nil {
+			panicRed(internal.WrapError(err))
+		}
+		_credential.awsConfig = &awsConfig
+	}
+
+	if awsRegion != "" {
+		_credential.awsConfig.Region = awsRegion
+	}
+	if _credential.awsConfig.Region == "" {
+		askRegion, err := AskRegion(context.Background(), *_credential.awsConfig)
+		if err != nil {
+			panicRed(internal.WrapError(err))
+		}
+		_credential.awsConfig.Region = askRegion.Name
+	}
+	color.Green("region (%s)", _credential.awsConfig.Region)
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringP("profile", "p", "", `[optional] if you having multiple aws profiles, it is one of profiles (default is AWS_PROFILE environment variable or default)`)
+	rootCmd.PersistentFlags().StringP("region", "r", "", `[optional] it is region in AWS would like to do something`)
+
+	rootCmd.InitDefaultVersionFlag()
+
+	viper.BindPFlag("profile", rootCmd.PersistentFlags().Lookup("profile"))
+	viper.BindPFlag("region", rootCmd.PersistentFlags().Lookup("region"))
+}
