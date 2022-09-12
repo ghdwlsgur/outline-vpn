b440f133f176dabddac80190b763dca78f996e84
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-12 16:20:01 +0900

    edit func PrintReady

--- internal/client.go
+++ internal/client.go
@@ -79,6 +79,6 @@ func DeleteStartSession(ctx context.Context, cfg aws.Config, input *ssm.Terminat
 	return err
 }
 
-func PrintReady(cmd, region, content, ami string) {
-	fmt.Printf("[%s] region: %s, %s: %s\n", color.GreenString(cmd), color.HiYellowString(region), content, color.HiYellowString(ami))
+func PrintReady(cmd, region, title, content string) {
+	fmt.Printf("%s region: %s, %s: %s\n", color.GreenString(cmd), color.HiYellowString(region), title, color.HiGreenString(content))
 }
