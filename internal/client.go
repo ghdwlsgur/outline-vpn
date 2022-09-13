0aa286bad8f7bd9461b605a1b51d47434031833b
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-13 16:43:31 +0900

    find ec2 all of region

--- internal/client.go
+++ internal/client.go
@@ -8,6 +8,7 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2_types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/fatih/color"
 )
@@ -23,6 +24,8 @@ var (
 		"sa-east-1",
 		"us-east-1", "us-east-2", "us-west-1", "us-west-2",
 	}
+
+	instanceList = make(map[string]*EC2)
 )
 
 type (
@@ -30,6 +33,45 @@ type (
 		Name string
 	}
 )
+
+func FindTagInstanceAllRegion(ctx context.Context, cfg aws.Config) (map[string]*EC2, error) {
+
+	client := ec2.NewFromConfig(cfg)
+
+	for _, region := range defaultAwsRegions {
+		defaultInstanceTagName = fmt.Sprintf("govpn-ec2-%s", region)
+
+		output, err := client.DescribeInstances(ctx,
+			&ec2.DescribeInstancesInput{
+				Filters: []ec2_types.Filter{
+					{Name: aws.String("instance-state-name"), Values: []string{"running"}},
+					{Name: aws.String("tag:Name"), Values: []string{defaultInstanceTagName}},
+				},
+			},
+		)
+		if err != nil {
+			return instanceList, err
+		}
+
+		for _, reservations := range output.Reservations {
+			for _, ec2 := range reservations.Instances {
+				instanceList[region] = &EC2{
+					Existence:    true,
+					Id:           aws.ToString(ec2.InstanceId),
+					PublicIP:     aws.ToString(ec2.PublicIpAddress),
+					LaunchTime:   aws.ToTime(ec2.LaunchTime),
+					InstanceType: aws.ToString((*string)(&ec2.InstanceType)),
+				}
+			}
+		}
+	}
+
+	for region, instance := range instanceList {
+		fmt.Printf("Instance List:\n[%s]\n%s\n%s\n%s\n%s\n", region, color.GreenString(instance.Id), instance.InstanceType, instance.PublicIP, instance.LaunchTime)
+	}
+
+	return instanceList, nil
+}
 
 func AskRegion(ctx context.Context, cfg aws.Config) (*Region, error) {
 	var regions []string
@@ -80,5 +122,5 @@ func DeleteStartSession(ctx context.Context, cfg aws.Config, input *ssm.Terminat
 }
 
 func PrintReady(cmd, region, title, content string) {
-	fmt.Printf("%s region: %s, %s: %s\n", color.GreenString(cmd), color.HiYellowString(region), title, color.HiGreenString(content))
+	fmt.Printf("%s aws-connect-region: %s, %s: %s\n", color.GreenString(cmd), color.HiYellowString(region), title, color.HiGreenString(content))
 }
