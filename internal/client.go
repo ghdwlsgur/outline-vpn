142778d944f62a24c3dd432a770dacbd75c38570
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-20 16:06:10 +0900

    📌 delete FindTagInstanceAllRegion

--- internal/client.go
+++ internal/client.go
@@ -9,7 +9,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	ec2_types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/aws/aws-sdk-go-v2/service/ssm"
 	"github.com/fatih/color"
 )
 
@@ -24,8 +23,6 @@ var (
 		"sa-east-1",
 		"us-east-1", "us-east-2", "us-west-1", "us-west-2",
 	}
-
-	instanceList = make(map[string]*EC2)
 )
 
 type (
@@ -33,45 +30,6 @@ type (
 		Name string
 	}
 )
-
-func FindTagInstanceAllRegion(ctx context.Context, cfg aws.Config) (map[string]*EC2, error) {
-
-	client := ec2.NewFromConfig(cfg)
-
-	for _, region := range defaultAwsRegions {
-		defaultInstanceTagName = fmt.Sprintf("govpn-ec2-%s", region)
-
-		output, err := client.DescribeInstances(ctx,
-			&ec2.DescribeInstancesInput{
-				Filters: []ec2_types.Filter{
-					{Name: aws.String("instance-state-name"), Values: []string{"running"}},
-					{Name: aws.String("tag:Name"), Values: []string{defaultInstanceTagName}},
-				},
-			},
-		)
-		if err != nil {
-			return instanceList, err
-		}
-
-		for _, reservations := range output.Reservations {
-			for _, ec2 := range reservations.Instances {
-				instanceList[region] = &EC2{
-					Existence:    true,
-					Id:           aws.ToString(ec2.InstanceId),
-					PublicIP:     aws.ToString(ec2.PublicIpAddress),
-					LaunchTime:   aws.ToTime(ec2.LaunchTime),
-					InstanceType: aws.ToString((*string)(&ec2.InstanceType)),
-				}
-			}
-		}
-	}
-
-	for region, instance := range instanceList {
-		fmt.Printf("Instance List:\n[%s]\n%s\n%s\n%s\n%s\n", region, color.GreenString(instance.Id), instance.InstanceType, instance.PublicIP, instance.LaunchTime)
-	}
-
-	return instanceList, nil
-}
 
 func AskRegion(ctx context.Context, cfg aws.Config) (*Region, error) {
 	var regions []string
@@ -106,19 +64,22 @@ func AskRegion(ctx context.Context, cfg aws.Config) (*Region, error) {
 	return &Region{Name: region}, nil
 }
 
-func CreateStartSession(ctx context.Context, cfg aws.Config, input *ssm.StartSessionInput) (*ssm.StartSessionOutput, error) {
-	client := ssm.NewFromConfig(cfg)
+func CreateTags(ctx context.Context, cfg aws.Config, id *string, tagName string) error {
+	client := ec2.NewFromConfig(cfg)
 
-	return client.StartSession(ctx, input)
-}
+	_, err := client.CreateTags(ctx,
+		&ec2.CreateTagsInput{
+			Resources: []string{aws.ToString(id)},
+			Tags: []ec2_types.Tag{
+				{Key: aws.String("Name"), Value: aws.String(tagName)},
+			},
+		},
+	)
+	if err != nil {
+		return err
+	}
 
-func DeleteStartSession(ctx context.Context, cfg aws.Config, input *ssm.TerminateSessionInput) error {
-	client := ssm.NewFromConfig(cfg)
-	fmt.Printf("%s %s \n", color.YellowString("Delete Session"),
-		color.YellowString(aws.ToString(input.SessionId)))
-
-	_, err := client.TerminateSession(ctx, input)
-	return err
+	return nil
 }
 
 func PrintReady(cmd, region, title, content string) {
