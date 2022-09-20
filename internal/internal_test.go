d6e12829a75c9de1f2e9be0ead620d8cfcba4bb0
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-20 16:07:18 +0900

    ✅ internal testMain (all pass)

--- internal/internal_test.go
+++ internal/internal_test.go
@@ -1,13 +1,8 @@
-/*
-Copyright © 2020 gjbae1212
-Released under the MIT license.
-(https://github.com/gjbae1212/gossm)
-*/
-
 package internal
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -20,6 +15,8 @@ var (
 	mockAwsKey    string
 	mockAwsSecret string
 	mockRegion    string
+
+	_credentialWithTemporary = fmt.Sprintf("%s_temporary", config.DefaultSharedCredentialsFilename())
 )
 
 func TestMain(m *testing.M) {
@@ -28,21 +25,50 @@ func TestMain(m *testing.M) {
 	}
 
 	mockProfile = "default"
-	filename := filepath.Join(os.Getenv("HOME"), ".aws/credentials")
-	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		os.Exit(0)
+
+	if _, err := os.Stat(_credentialWithTemporary); os.IsNotExist(err) {
+		filename := filepath.Join(os.Getenv("HOME"), "/.aws/credentials")
+
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			os.Exit(0)
+		} else {
+			cfg, err := config.LoadDefaultConfig(context.Background(),
+				config.WithSharedConfigProfile(mockProfile),
+				config.WithSharedConfigFiles([]string{config.DefaultSharedConfigFilename()}),
+				config.WithSharedCredentialsFiles([]string{config.DefaultSharedCredentialsFilename()}),
+			)
+			if err != nil {
+				panic(err)
+			}
+
+			cred, err := cfg.Credentials.Retrieve(context.Background())
+			if err != nil {
+				panic(err)
+			}
+
+			mockAwsKey = cred.AccessKeyID
+			mockAwsSecret = cred.SecretAccessKey
+			mockRegion = cfg.Region
+			os.Exit(m.Run())
+		}
+
 	} else {
-		cfg, err := NewSharedConfig(context.Background(), mockProfile,
-			[]string{config.DefaultSharedConfigFilename()},
-			[]string{config.DefaultSharedCredentialsFilename()},
+		os.Setenv("AWS_SHARED_CREDENTIALS_FILE", _credentialWithTemporary)
+
+		cfg, err := config.LoadDefaultConfig(context.Background(),
+			config.WithSharedConfigProfile(mockProfile),
+			config.WithSharedConfigFiles([]string{}),
+			config.WithSharedCredentialsFiles([]string{_credentialWithTemporary}),
 		)
 		if err != nil {
-			os.Exit(0)
+			panic(err)
 		}
+
 		cred, err := cfg.Credentials.Retrieve(context.Background())
 		if err != nil {
 			panic(err)
 		}
+
 		mockAwsKey = cred.AccessKeyID
 		mockAwsSecret = cred.SecretAccessKey
 		mockRegion = cfg.Region
