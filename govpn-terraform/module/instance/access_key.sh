a453cf2811778918013255b80c067a30f33b9462
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-27 17:43:39 +0900

    delete govpn-terraform (execute error)

--- govpn-terraform/module/instance/access_key.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash 
-
-
-region="$1"
-path=$(echo ../../terraform.tfstate.d/"$region")
-
-
-accessKey=$(jq ".OutlineClientAccessKey" "$path"/outline.json | tr -d '"' && jq ".OutlineClientAccessKey" "$path"/outline.json | tr -d '"' | pbcopy)
-jq -n --arg accessKey "$accessKey" '{"accessKey": $accessKey}' 
--- govpn-terraform/module/instance/output.tf
+++
@@ -1,9 +0,0 @@
-
-
-output "OutlineClientAccessKey" {
-  value = data.external.access_key.result["accessKey"]
-}
-
-output "Region" {
-  value = var.aws_region
-}
