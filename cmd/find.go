b42dfee45d963698ebfcc2f1fb50ac90615b06a7
HongJinHyeok <redmax45@naver.com>
2023-04-30 22:20:14 +0900

    create screen that will be shown to the client

--- cmd/find.go
+++ cmd/find.go
@@ -3,7 +3,10 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/briandowns/spinner"
+	"github.com/fatih/color"
 	"github.com/ghdwlsgur/govpn/internal"
 	"github.com/spf13/cobra"
 )
@@ -16,12 +19,35 @@ var (
 		Run: func(_ *cobra.Command, _ []string) {
 			ctx := context.Background()
 
+			ctx, cancel := context.WithTimeout(ctx, time.Minute)
+			defer cancel()
+
+			s := spinner.New(spinner.CharSets[8], 100*time.Millisecond)
+			s.UpdateCharSet(spinner.CharSets[59])
+			s.Color("fgHiGreen")
+			s.Restart()
+			s.Prefix = color.HiGreenString("Finding EC2 Instance ")
+
 			regionList, err := internal.FindTagInstance(ctx, *_credential.awsConfig)
 			if err != nil {
 				panicRed(err)
 			}
-
+			s.Stop()
 			fmt.Println(regionList)
+
+			go func() {
+				cancel()
+			}()
+
+		delay:
+			for {
+				select {
+				case <-time.After(time.Second):
+				case <-ctx.Done():
+					break delay
+				}
+			}
+
 		},
 	}
 )
