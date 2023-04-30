61eed20a9ba6d38f6d0243314b66ab8336d590c1
HongJinHyeok <redmax45@naver.com>
2023-04-30 21:12:13 +0900

    query from available region (sts)

--- internal/client.go
+++ internal/client.go
@@ -12,19 +12,6 @@ import (
 	"github.com/fatih/color"
 )
 
-var (
-	defaultAwsRegions = []string{
-		"af-south-1",
-		"ap-east-1", "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ap-southeast-3",
-		"ca-central-1",
-		"cn-north-1", "cn-northwest-1",
-		"eu-central-1", "eu-north-1", "eu-south-1", "eu-west-1", "eu-west-2", "eu-west-3",
-		"me-south-1", "me-central-1",
-		"sa-east-1",
-		"us-east-1", "us-east-2", "us-west-1", "us-west-2",
-	}
-)
-
 type (
 	Region struct {
 		Name string
@@ -35,12 +22,21 @@ func AskRegion(ctx context.Context, cfg aws.Config) (*Region, error) {
 	var regions []string
 	client := ec2.NewFromConfig(cfg)
 
-	output, err := client.DescribeRegions(ctx, &ec2.DescribeRegionsInput{
-		AllRegions: aws.Bool(true),
-	})
+	// legacy [all region]
+	// output, err := client.DescribeRegions(ctx, &ec2.DescribeRegionsInput{
+	// 	AllRegions: aws.Bool(true),
+	// })
+
+	// sts: available
+	output, err := client.DescribeRegions(ctx,
+		&ec2.DescribeRegionsInput{
+			Filters: []ec2_types.Filter{
+				{Name: aws.String("endpoint"), Values: []string{"*"}},
+			},
+		})
+
 	if err != nil {
-		regions = make([]string, len(defaultAwsRegions))
-		copy(regions, defaultAwsRegions)
+		return nil, err
 	} else {
 		regions = make([]string, 0, len(output.Regions))
 		for _, region := range output.Regions {
