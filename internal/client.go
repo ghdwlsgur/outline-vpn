386340754805fb00f6b55acfadb85df0da023b79
HongJinHyeok <redmax45@naver.com>
2023-05-01 20:09:18 +0900

    wrapper outline VPN

--- internal/client.go
+++ internal/client.go
@@ -2,7 +2,10 @@ package internal
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
+	"os"
 	"sort"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -10,6 +13,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	ec2_types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/fatih/color"
+	"github.com/go-resty/resty/v2"
 )
 
 type (
@@ -17,6 +21,223 @@ type (
 		Name string
 	}
 )
+
+type OutlineInfo struct {
+	ManagementUdpPort int    `json:"ManagementUdpPort"`
+	VpnTcpUdpPort     int    `json:"VpnTcpUdpPort"`
+	ApiURL            string `json:"ApiUrl"`
+	CertSha256        string `json:"CertSha256"`
+}
+
+type DataLimit struct {
+	Bytes int `json:"bytes"`
+}
+
+type AccessKey struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Password  string `json:"password"`
+	Port      int    `json:"port"`
+	Method    string `json:"method"`
+	DataLimit DataLimit
+	AccessURL string `json:"accessUrl"`
+}
+
+type AccessKeys struct {
+	Keys []AccessKey `json:"accessKeys"`
+}
+
+func (l *AccessKeys) GetAccessUrlList() []string {
+	var result []string
+	result = make([]string, 0, len(l.Keys))
+	for _, v := range l.Keys {
+		result = append(result, v.AccessURL)
+	}
+	return result
+}
+
+func readOutlineInfo(region string) (*OutlineInfo, error) {
+	outlineJsonPath := func(path, region string) string {
+		return path + region + "/outline.json"
+	}("/opt/homebrew/lib/govpn/govpn-terraform/terraform.tfstate.d/", region)
+
+	b, err := os.ReadFile(outlineJsonPath)
+	if err != nil {
+		return nil, err
+	}
+
+	var outlineInfo OutlineInfo
+	err = json.Unmarshal(b, &outlineInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &outlineInfo, nil
+}
+
+func GetCertSha256(region string) (string, error) {
+	outlineInfo, err := readOutlineInfo(region)
+	if err != nil {
+		return "", err
+	}
+
+	return outlineInfo.CertSha256, nil
+}
+
+func GetApiURL(region string) (string, error) {
+	outlineInfo, err := readOutlineInfo(region)
+	if err != nil {
+		return "", err
+	}
+	return outlineInfo.ApiURL, nil
+}
+
+func CreateAccessKey(region string) error {
+	apiURL, err := GetApiURL(region)
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("%s/%s", apiURL, "access-keys")
+
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		Post(url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() == 200 {
+		return nil
+	}
+	return err
+}
+
+func GetAccessKeys(region string) ([]string, error) {
+	apiURL, err := GetApiURL(region)
+	if err != nil {
+		return nil, err
+	}
+	url := fmt.Sprintf("%s/%s", apiURL, "access-keys")
+
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var accessKeys AccessKeys
+	if resp.StatusCode() == 200 {
+		err = json.Unmarshal(resp.Body(), &accessKeys)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return accessKeys.GetAccessUrlList(), nil
+}
+
+func DeleteAccessKey(region string, id int) error {
+	apiURL, err := GetApiURL(region)
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("%s/%s/%d", apiURL, "access-keys", id)
+
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+
+	resp, err := client.R().Delete(url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() == 204 {
+		return nil
+	}
+	return err
+}
+
+func RenameAccessKey(region string, id int, name string) error {
+	apiURL, err := GetApiURL(region)
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("%s/%s/%d/%s", apiURL, "access-keys", id, "name")
+
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+
+	putData := map[string]string{
+		"name": name,
+	}
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(putData).
+		Put(url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() == 204 {
+		return nil
+	}
+	return err
+}
+
+func AddDataLimitAccessKey(region string, id int, limit int) error {
+	apiURL, err := GetApiURL(region)
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("%s/%s/%d/%s", apiURL, "access-keys", id, "data-limit")
+
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+
+	putData := map[string]map[string]int{
+		"limit": {
+			"bytes": limit,
+		},
+	}
+	resp, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(putData).
+		Put(url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() == 204 {
+		return nil
+	}
+	return err
+}
+
+func DeleteDataLimitAccessKey(region string, id int) error {
+	apiURL, err := GetApiURL(region)
+	if err != nil {
+		return err
+	}
+	url := fmt.Sprintf("%s/%s/%d/%s", apiURL, "access-keys", id, "data-limit")
+
+	client := resty.New()
+	client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+
+	resp, err := client.R().Delete(url)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode() == 204 {
+		return nil
+	}
+	return err
+}
 
 func AskRegion(ctx context.Context, cfg aws.Config) (*Region, error) {
 	var regions []string
--- internal/client_test.go
+++ internal/client_test.go
@@ -1,1 +1,55 @@
 package internal
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetAccessKeys(t *testing.T) {
+	// assert := assert.New(t)
+
+	test, err := GetAccessKeys("ap-northeast-1")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(test)
+}
+
+func TestDeleteAccessKey(t *testing.T) {
+	DeleteAccessKey("ap-northeast-1", 4)
+}
+
+func TestRenameAccessKey(t *testing.T) {
+	RenameAccessKey("ap-northeast-1", 5, "jinhyeok")
+}
+
+func TestAddDataLimitAccessKey(t *testing.T) {
+	AddDataLimitAccessKey("ap-northeast-1", 5, 123456)
+}
+
+func TestDeleteDataLimitAccessKey(t *testing.T) {
+	DeleteDataLimitAccessKey("ap-northeast-1", 7)
+}
+
+func TestCreateAccessKey(t *testing.T) {
+	CreateAccessKey("ap-northeast-1")
+}
+
+func TestGetSha256(t *testing.T) {
+	result, err := GetCertSha256("ap-northeast-1")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+}
+
+func TestGetApiURL(t *testing.T) {
+	result, err := GetApiURL("ap-northeast-1")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
+}
